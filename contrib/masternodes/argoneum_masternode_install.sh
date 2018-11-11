8a7e54c8025c607931eef4a2472610ad8dd50848
argoneum <argoneum@argoneum.net>
2018-11-11 15:43:27 +0300

    MN install: update script to install/upgrade to v1.1.0.0

--- contrib/masternodes/argoneum_masternode_install.sh
+++ contrib/masternodes/argoneum_masternode_install.sh
@@ -27,7 +27,7 @@ COIN_CLI="${coin_name}-cli"
 TMP_FOLDER=$(mktemp -d)
 COIN_PATH='/usr/local/bin/'
 KERN_ARCH=$(getconf LONG_BIT)
-COIN_TGZ="https://github.com/argoneum/argoneum/releases/download/v1.0.0.0/argoneum-1.0.0-linux${KERN_ARCH}.tar.gz"
+COIN_TGZ="https://github.com/argoneum/argoneum/releases/download/v1.1.0.0/argoneum-1.1.0-linux${KERN_ARCH}.tar.gz"
 COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
 
 NODEIP=$(curl -s4 icanhazip.com)
