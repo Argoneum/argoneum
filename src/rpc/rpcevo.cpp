27ba1e7f901dd0be42f4ad3d8fbd9d3f137ad392
argoneum <argoneum@argoneum.net>
2020-01-11 22:03:54 +0300

    Deterministic masternodes: fix collateral value

--- src/rpc/rpcevo.cpp
+++ src/rpc/rpcevo.cpp
@@ -297,7 +297,7 @@ void protx_register_fund_help()
 {
     throw std::runtime_error(
             "protx register_fund \"collateralAddress\" \"ipAndPort\" \"ownerAddress\" \"operatorPubKey\" \"votingAddress\" operatorReward \"payoutAddress\" ( \"fundAddress\" )\n"
-            "\nCreates, funds and sends a ProTx to the network. The resulting transaction will move 1000 Argoneum\n"
+            "\nCreates, funds and sends a ProTx to the network. The resulting transaction will move 10000 Argoneum\n"
             "to the address specified by collateralAddress and will then function as the collateral of your\n"
             "masternode.\n"
             "A few of the limitations you see in the arguments are temporary and might be lifted after DIP3\n"
@@ -404,7 +404,7 @@ UniValue protx_register(const JSONRPCRequest& request)
 
     size_t paramIdx = 1;
 
-    CAmount collateralAmount = 1000 * COIN;
+    CAmount collateralAmount = 10000 * COIN;
 
     CMutableTransaction tx;
     tx.nVersion = 3;
