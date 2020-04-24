fbcf45bfb0e1c54042358b5dc2186dcfe6cd20a1
argoneum <argoneum@argoneum.net>
2020-04-24 11:54:19 +0300

    Use registered BIP44 coin code. WARNING: it changes HD wallet addresses
    Using this version will change the HD wallet address chain. If you
    use HD wallet, you will have other addresses for the same phrase.
    It is painful but necessary change in order to go forward officially.
    Please keep an old version around to send all your coins to new addresses
    generated by this version using the same old mnemonic.
    
    If you did not enable HD wallet, you don't have to do any changes
    or update to this version.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -317,8 +317,8 @@ public:
         // Argoneum BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        // Argoneum BIP44 coin type
-        nExtCoinType = 5;
+        // Argoneum BIP44 coin type, https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+        nExtCoinType = 421;
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
